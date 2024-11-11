d0876106bada5bc0912453f1679754c8f65acd90
Alexandru Raul Cenan <alexandru.cenan@scalefast.com>
2024-11-12 00:52:16 +0100

    chore: remove .gitkeep files and add FruitName.php and VegetableName.php

---
+++ src/Fruit/Domain/FruitName.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VeggieVibe\Fruit\Domain;
+
+use VeggieVibe\Shared\Domain\ValueObject\StringValueObject;
+
+final class FruitName extends StringValueObject {}
---
+++ src/Vegetable/Domain/VegetableName.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VeggieVibe\Vegetable\Domain;
+
+use VeggieVibe\Shared\Domain\ValueObject\StringValueObject;
+
+final class VegetableName extends StringValueObject {}
