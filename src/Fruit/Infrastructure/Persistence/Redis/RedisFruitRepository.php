a5ae268ed530bf59947cfca49474bd810db7a026
Alexandru Raul Cenan <alexandru.cenan@scalefast.com>
2024-11-13 11:32:17 +0100

    feat: basic behaviour for RedisVegetableRepository and RedisFruitRepository

---
+++ src/Fruit/Infrastructure/Persistence/Redis/RedisFruitRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VeggieVibe\Fruit\Infrastructure\Persistence\Redis;
+
+use VeggieVibe\Fruit\Domain\Fruit;
+use VeggieVibe\Fruit\Domain\FruitId;
+use VeggieVibe\Fruit\Domain\FruitRepository;
+use VeggieVibe\Shared\Domain\ValueObject\ItemType;
+use VeggieVibe\Shared\Infrastructure\Persistence\Redis\RedisRepository;
+
+
+final class RedisFruitRepository extends RedisRepository implements FruitRepository
+{
+    protected const PREFIX = ItemType::FRUIT;
+
+    public function save(Fruit $item): void
+    {
+        $this->persist($item);
+    }
+
+    public function findById(FruitId $id): ?Fruit
+    {
+        return $this->toFruit($this->searchById($id));
+    }
+
+    public function delete(FruitId $id): void
+    {
+        $this->remove($id);
+    }
+
+    private function toFruit($data) {
+        return $data;
+    }
+}
---
+++ src/Vegetable/Infrastructure/Persistence/Redis/RedisVegetableRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VeggieVibe\Vegetable\Infrastructure\Persistence\Redis;
+
+use VeggieVibe\Shared\Domain\ValueObject\ItemType;
+use VeggieVibe\Vegetable\Domain\Vegetable;
+use VeggieVibe\Vegetable\Domain\VegetableId;
+use VeggieVibe\Vegetable\Domain\VegetableRepository;
+use VeggieVibe\Shared\Infrastructure\Persistence\Redis\RedisRepository;
+
+final class RedisVegetableRepository extends RedisRepository implements VegetableRepository
+{
+    protected const PREFIX = ItemType::VEGETABLE;
+
+    public function save(Vegetable $item): void
+    {
+        $this->persist($item);
+    }
+
+    public function findById(VegetableId $id): ?Vegetable
+    {
+        return $this->toVegetable($this->searchById($id));
+    }
+
+    public function delete(VegetableId $id): void
+    {
+        $this->remove($id);
+    }
+
+    private function toVegetable($data) {
+        return $data;
+    }
+}
