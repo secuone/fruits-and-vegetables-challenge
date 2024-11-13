92c60d5ffa2708976e0edb44cdf436aa49275b70
Alexandru Raul Cenan <alexandru.cenan@scalefast.com>
2024-11-13 17:20:33 +0100

    feat: improve itemtype

--- src/Shared/Domain/ValueObject/ItemType.php
+++ src/Shared/Domain/ValueObject/ItemType.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace VeggieVibe\Shared\Domain\ValueObject;
 
+use VeggieVibe\Fruit\Domain\Fruit;
+use VeggieVibe\Vegetable\Domain\Vegetable;
+
 enum ItemType: string
 {
     case FRUIT = 'fruit';
     case VEGETABLE = 'vegetable';
+
+    public final function class(): string
+    {
+        return match ($this) {
+            self::FRUIT => Fruit::class,
+            self::VEGETABLE => Vegetable::class,
+        };
+    }
 }
