95ed76198355b9dd4ff1a5ab40ba7f053211a05b
Alexandru Raul Cenan <alexandru.cenan@scalefast.com>
2024-11-13 11:29:04 +0100

    feat: basic behaviour for RedisRepository

--- src/Shared/Infrastructure/Persistence/Redis/RedisRepository.php
+++ src/Shared/Infrastructure/Persistence/Redis/RedisRepository.php
@@ -5,20 +5,36 @@ declare(strict_types=1);
 namespace VeggieVibe\Shared\Infrastructure\Persistence\Redis;
 
 use Redis;
+use VeggieVibe\Shared\Domain\Item;
+use VeggieVibe\Shared\Domain\Uuid;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 abstract class RedisRepository
 {
-	public function __construct(private readonly Redis $client) {}
+	public function __construct(
+        private readonly Redis $client,
+        private readonly NormalizerInterface $serializer
+    ) {}
 
-    protected function persist() {
-        return true;
+    protected function persist(Item $item): void
+    {
+        $this->client->hSet(
+            static::PREFIX->value,
+            $item->id()->value(),
+            $this->serializer->serialize($item, 'json')
+        );
     }
 
-    protected function remove() {
-        return true;
+    protected function searchById(Uuid $uuid): ?Item
+    {
+        return null;
     }
 
-    protected function find() {
-        return true;
+    protected function remove(Uuid $uuid): void
+    {
+        $this->client->hDel(
+            static::PREFIX,
+            $uuid->value()
+        );
     }
 }
