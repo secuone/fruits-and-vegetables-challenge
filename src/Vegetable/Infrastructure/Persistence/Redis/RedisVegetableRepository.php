3bded6b6b4f0ce8e5af26161c4fd5bcd14f7d624
Alexandru Raul Cenan <alexandru.cenan@scalefast.com>
2024-11-13 17:16:14 +0100

    feat: changes in redisvegetablerepository

--- src/Vegetable/Infrastructure/Persistence/Redis/RedisVegetableRepository.php
+++ src/Vegetable/Infrastructure/Persistence/Redis/RedisVegetableRepository.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace VeggieVibe\Vegetable\Infrastructure\Persistence\Redis;
 
-use VeggieVibe\Shared\Domain\ValueObject\ItemType;
 use VeggieVibe\Vegetable\Domain\Vegetable;
 use VeggieVibe\Vegetable\Domain\VegetableId;
 use VeggieVibe\Vegetable\Domain\VegetableRepository;
+use VeggieVibe\Shared\Domain\ValueObject\ItemType;
 use VeggieVibe\Shared\Infrastructure\Persistence\Redis\RedisRepository;
 
 final class RedisVegetableRepository extends RedisRepository implements VegetableRepository
@@ -21,16 +21,12 @@ final class RedisVegetableRepository extends RedisRepository implements Vegetabl
 
     public function findById(VegetableId $id): ?Vegetable
     {
-        return $this->toVegetable($this->searchById($id));
+        return $this->searchById($id, ItemType::VEGETABLE);
     }
 
     public function delete(VegetableId $id): void
     {
         $this->remove($id);
-    }
-
-    private function toVegetable($data) {
-        return $data;
     }
 
     public function deleteAll(): void
