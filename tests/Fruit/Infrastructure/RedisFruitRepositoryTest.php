c270d89a354dad27845fa0cc8d65a727607149d9
Alexandru Raul Cenan <alexandru.cenan@scalefast.com>
2024-11-13 12:51:45 +0100

    test: add RedisFruitRepositoryTest

---
+++ tests/Fruit/Infrastructure/RedisFruitRepositoryTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VeggieVibe\Tests\Fruit\Infrastructure;
+
+use Redis;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Serializer\SerializerInterface;
+use VeggieVibe\Shared\Domain\ValueObject\ItemType;
+use VeggieVibe\Fruit\Infrastructure\Persistence\Redis\RedisFruitRepository;
+use VeggieVibe\Tests\Shared\Infrastructure\NormalizerMock;
+use VeggieVibe\Tests\Fruit\Domain\FruitMother;
+
+final class RedisFruitRepositoryTest extends TestCase
+{
+    private readonly RedisFruitRepository $repository;
+    private readonly Redis $redisClientMock;
+    private readonly SerializerInterface $normalizerMock;
+
+    public function setUp(): void
+    {
+        $this->redisClientMock = $this->createMock(Redis::class);
+        $this->normalizerMock = $this->createMock(NormalizerMock::class);
+
+        $this->repository = new RedisFruitRepository(
+            $this->redisClientMock,
+            $this->normalizerMock
+        );
+    }
+
+	/** @test */
+	public function test_it_should_save_a_fruit(): void
+	{
+		$item = FruitMother::create();
+
+		$this->redisClientMock->expects($this->once())
+            ->method('hSet')
+            ->with(
+                $this->equalTo(ItemType::FRUIT->value),
+                $this->equalTo($item->id()->value()),
+                $this->anything()
+            );
+
+        $this->normalizerMock->expects($this->once())
+            ->method('serialize');
+        
+        $this->repository->save($item);
+	}
+
+    /** @test */
+    public function test_it_should_delete_all_fruits()
+    {
+        $this->redisClientMock->expects($this->once())
+        ->method('del')
+        ->with(ItemType::FRUIT->value);
+        
+        $this->repository->deleteAll();
+    }
+}
